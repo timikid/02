0b6eafd3e1d85c4c86ccadd99e561b5f6040bcb5
timikid <timikit@naver.com>
2020-09-10 22:47:23 +0900

    02

--- helloE.c
+++ helloE.c
@@ -3,8 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
+int a, b; //정수 변수 선언
+ 
 	printf("Hello, Sookmyung Electronics!\n");
-	return 0;
+    a = 10;
+    b = 20;
+    
+    printf("%d + %d = %d\n", a, b, a+b);
+    return 0;
 }
+
